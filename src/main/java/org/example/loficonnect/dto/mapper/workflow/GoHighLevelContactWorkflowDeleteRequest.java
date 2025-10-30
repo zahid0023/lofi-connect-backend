3740a34b2a8cbfbbb89861161b3f3aa791a39e2e
MDRobiulHassan <robiul110256@gmail.com>
2025-10-31 03:56:31 +0600

    Request Added

--- src/main/java/org/example/loficonnect/dto/mapper/workflow/GoHighLevelContactWorkflowDeleteRequest.java
+++ src/main/java/org/example/loficonnect/dto/mapper/workflow/GoHighLevelContactWorkflowDeleteRequest.java
@@ -1,7 +1,9 @@
 package org.example.loficonnect.dto.mapper.workflow;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Data;
 import org.example.loficonnect.dto.request.workflow.ContactWorkflowDeleteRequest;
 import org.example.loficonnect.util.DateTimeUtil;
@@ -13,13 +15,14 @@ import java.time.ZonedDateTime;
 @Data
 public class GoHighLevelContactWorkflowDeleteRequest {
 
+    @JsonAlias({"event_start_time"})
     private ZonedDateTime eventStartTime;
 
     private GoHighLevelContactWorkflowDeleteRequest() {
     }
 
-    public static GoHighLevelContactWorkflowDeleteRequest fromRequest(ContactWorkflowDeleteRequest request) {
-        GoHighLevelContactWorkflowDeleteRequest ghlRequest = new GoHighLevelContactWorkflowDeleteRequest();
+    public static GoHighLevelContactWorkflowDeleteRequest fromRequest(ContactWorkflowDeleteRequest request, ObjectMapper objectMapper) {
+        GoHighLevelContactWorkflowDeleteRequest ghlRequest = objectMapper.convertValue(request, GoHighLevelContactWorkflowDeleteRequest.class);
         ghlRequest.setEventStartTime(DateTimeUtil.toZonedDateTime(request.getEventDate(), request.getEventTime(), request.getTimeZone()));
         return ghlRequest;
     }
