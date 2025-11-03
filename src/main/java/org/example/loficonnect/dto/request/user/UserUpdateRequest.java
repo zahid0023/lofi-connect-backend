e2a008aeec0032a085b5341724f45363706e7272
MDRobiulHassan <robiul110256@gmail.com>
2025-11-03 23:26:46 +0600

    User Update Response Fixed

--- src/main/java/org/example/loficonnect/dto/request/user/UserUpdateRequest.java
+++ src/main/java/org/example/loficonnect/dto/request/user/UserUpdateRequest.java
@@ -21,8 +21,51 @@ public class UserUpdateRequest {
     private String role;
     private String companyId;
     private List<String> locationIds;
-    private Map<String, Boolean> permissions;
+    private Permissions permissions;
     private List<String> scopes;
     private List<String> scopesAssignedToOnly;
     private String profilePhoto;
+
+    @Data
+    @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+    public static class Permissions {
+        private Boolean campaignsEnabled;
+        private Boolean campaignsReadOnly;
+        private Boolean contactsEnabled;
+        private Boolean workflowsEnabled;
+        private Boolean workflowsReadOnly;
+        private Boolean triggersEnabled;
+        private Boolean funnelsEnabled;
+        private Boolean websitesEnabled;
+        private Boolean opportunitiesEnabled;
+        private Boolean dashboardStatsEnabled;
+        private Boolean bulkRequestsEnabled;
+        private Boolean appointmentsEnabled;
+        private Boolean reviewsEnabled;
+        private Boolean onlineListingsEnabled;
+        private Boolean phoneCallEnabled;
+        private Boolean conversationsEnabled;
+        private Boolean assignedDataOnly;
+        private Boolean adwordsReportingEnabled;
+        private Boolean membershipEnabled;
+        private Boolean facebookAdsReportingEnabled;
+        private Boolean attributionsReportingEnabled;
+        private Boolean settingsEnabled;
+        private Boolean tagsEnabled;
+        private Boolean leadValueEnabled;
+        private Boolean marketingEnabled;
+        private Boolean agentReportingEnabled;
+        private Boolean botService;
+        private Boolean socialPlanner;
+        private Boolean bloggingEnabled;
+        private Boolean invoiceEnabled;
+        private Boolean affiliateManagerEnabled;
+        private Boolean contentAiEnabled;
+        private Boolean refundsEnabled;
+        private Boolean recordPaymentEnabled;
+        private Boolean cancelSubscriptionEnabled;
+        private Boolean paymentsEnabled;
+        private Boolean communitiesEnabled;
+        private Boolean exportPaymentsEnabled;
+    }
 }
